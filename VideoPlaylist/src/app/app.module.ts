06fddfbffd4a84553d380e33fbea5bf876117245
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2019-03-03 19:53:51 +0100

    Videogular2 installed to have a video player for angular

--- VideoPlaylist/src/app/app.module.ts
+++ VideoPlaylist/src/app/app.module.ts
@@ -11,19 +11,29 @@ import { VideosService } from './services/videos.service';
 import { CustomTimePipe } from './pipes/custom-time.pipe';
 import { RouterModule, Routes } from '@angular/router';
 import { EmbedVideo } from 'ngx-embed-video';
+import {VgCoreModule} from 'videogular2/core';
+import {VgControlsModule} from 'videogular2/controls';
+import {VgOverlayPlayModule} from 'videogular2/overlay-play';
+import {VgBufferingModule} from 'videogular2/buffering';
+// import {SingleMediaPlayer} from './single-media-player';
  
 @NgModule({
   declarations: [
     AppComponent,
     VideoListComponent,
     VideoDetailsComponent,
-    CustomTimePipe
+    CustomTimePipe,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
     EmbedVideo.forRoot()
   ],
   providers: [VideosService],
