7866dc28ccc0a21a366d196ed641e829b89be623
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2019-03-03 08:51:40 +0100

    video-list & video-details component created

--- VideoPlaylist/src/app/app.module.ts
+++ VideoPlaylist/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VideoListComponent } from './components/video-list/video-list.component';
+import { VideoDetailsComponent } from './components/video-details/video-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoListComponent,
+    VideoDetailsComponent
   ],
   imports: [
     BrowserModule
