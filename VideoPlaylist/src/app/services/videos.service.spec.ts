2bd31c7db56473bbe1ff083308dcaa9fc19128bd
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2019-03-03 22:46:56 +0100

    tests for video-service added, code formatted

--- VideoPlaylist/src/app/services/videos.service.spec.ts
+++ VideoPlaylist/src/app/services/videos.service.spec.ts
@@ -1,15 +1,17 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
-import { VideosService } from './videos.service';
+import { TestBed, inject } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
-describe('VideosService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule],
-    providers: [VideosService]
-  }));
+import { VideosService } from "./videos.service";
 
-  it('should be created', () => {
-    const service: VideosService = TestBed.get(VideosService);
-    expect(service).toBeTruthy();
+describe("VideosService", () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [VideosService],
+      imports: [HttpClientTestingModule],
+    });
   });
+
+  it("should be created", inject([VideosService], (service: VideosService) => {
+    expect(service).toBeTruthy();
+  }));
 });
