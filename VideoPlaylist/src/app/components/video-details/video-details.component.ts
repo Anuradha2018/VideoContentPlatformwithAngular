8ebe8bf83105a2c58e7383f2bf85b606dcac1fd1
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2019-03-03 22:44:13 +0100

    unwanted code removed, code formated , indented for video detail ts file

--- VideoPlaylist/src/app/components/video-details/video-details.component.ts
+++ VideoPlaylist/src/app/components/video-details/video-details.component.ts
@@ -1,32 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { VideosService } from './../../services/videos.service';
-import { EmbedVideoService } from 'ngx-embed-video';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { VideosService } from "./../../services/videos.service";
 
 @Component({
-  selector: 'app-video-details',
-  templateUrl: './video-details.component.html',
-  styleUrls: ['./video-details.component.scss']
+  selector: "app-video-details",
+  templateUrl: "./video-details.component.html",
+  styleUrls: ["./video-details.component.scss"],
 })
 export class VideoDetailsComponent implements OnInit {
   allVideos = [];
   videoId: number;
   videoDetail = null;
   videoUrl: string;
+
   constructor(
     public route: ActivatedRoute,
-    public videoService: VideosService,
-    private embedService: EmbedVideoService
-  ) {
-    // console.log(this.embedService.embed(this.videoDetail.videoUrl));
-   }
+    public videoService: VideosService
+  ) {}
 
   ngOnInit() {
     this.videoService.fetchVideos().subscribe(data => {
       this.allVideos = this.allVideos.concat(data);
 
       this.route.params.subscribe(params => {
-        this.videoId = +params['id'];
+        this.videoId = +params["id"];
         if (this.allVideos.length > 0) {
           this.videoDetail = this.allVideos[this.videoId];
           this.videoUrl = this.videoDetail.videoUrl;
@@ -34,5 +31,4 @@ export class VideoDetailsComponent implements OnInit {
       });
     });
   }
-
 }
